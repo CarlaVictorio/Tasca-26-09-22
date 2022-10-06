1b2cd7916bf494c9609230522c02a677fe7865f7
CarlaVictorio <101456362+CarlaVictorio@users.noreply.github.com>
2022-10-06 12:24:45 +0200

    6/10/22

--- src/main/java/LaMevaCua.java
+++ src/main/java/LaMevaCua.java
@@ -1,6 +1,11 @@
 public class LaMevaCua<E>  implements Queue<E> {
 
     E[] elems;
+    private int p;
+
+    public LaMevaCua(int i) {
+        elems = (E[]) new Object[i];
+    }
 
     @Override
     public void push(E e) throws FullQueueException {
@@ -9,18 +14,26 @@ public class LaMevaCua<E>  implements Queue<E> {
         elems[p++] = e;
     }
 
+    private boolean isFull() {
+        return (this.p >= elems.length);
+    }
+
     @Override
     public E pop() throws EmptyQueueException {
-        if(isEmpty()) throw EmptyQueueException;
+        if(isEmpty()) throw new EmptyQueueException();
         E primer = elems[0];
         //Desplaçament esquerra
-        for (int i; i<p ; i++) {
+        for (int i=0; i<p ; i++) {
             elems[i] = elems[i + 1];
         }
 
         this.p--;
         return primer;
         }
+
+    private boolean isEmpty() {
+        return (this.p ==0 );
+    }
 
     @Override
     public int size() {
